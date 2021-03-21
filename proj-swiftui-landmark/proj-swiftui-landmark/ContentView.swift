8ab9598aadf74485fbf7fb32504e7284e86223cc
smilemike <419854846@qq.com>
2021-03-21 15:43:22 +0800

    init project

---
+++ proj-swiftui-landmark/proj-swiftui-landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proj-swiftui-landmark
+//
+//  Created by smilemike on 2021/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ proj-swiftui-landmark/proj-swiftui-landmark/proj_swiftui_landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  proj_swiftui_landmarkApp.swift
+//  proj-swiftui-landmark
+//
+//  Created by smilemike on 2021/3/21.
+//
+
+import SwiftUI
+
+@main
+struct proj_swiftui_landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
